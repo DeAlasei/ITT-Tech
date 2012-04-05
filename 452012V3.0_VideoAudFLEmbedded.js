2238cb92a19448516f9548c605646b6f5e3dafea
Dana De Alasei <dalasei@mobiquity.com>
2012-04-05 17:07:17 -0400

    User Story 051 :enclose learning assets in an image tag and position them on right side of page

---
+++ 452012V3.0_VideoAudFLEmbedded.js
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>IntelliCourse App Server Demonstration</title>
+    <script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="path/DeAlasei/ITTModal/css/prettyPhoto.css" type="text/css" media= "screen/>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="path/DeAlasei/ITTModal/js/prettyPhoto.js"</script>
+   
+             
+</head>
+<body>
+<script type="text/javascript" charset="utf-8">
+  $(document).ready(function(){
+    $("area[rel^='prettyPhoto']").prettyPhoto();
+            allow_resize: true; /* Allow resize of images  larger that viewer */
+            autoplay: false /*Automatically start videos: True/False*/
+            ie6_fallback: true;
+  });
+</script>
+
+<div style="font-size:24px;font-weight:bold">
+Text Headline: All guids supplied by Drupal
+</div>
+
+<div style="float:left;width:50%;margin:0;padding:0;">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis, enim in volutpat blandit, nunc augue ultricies purus, vitae tincidunt magna lacus in mauris. Fusce pretium erat vitae ante auctor quis luctus nisl euismod. Integer varius vehicula risus, nec tincidunt justo sagittis at. Nunc hendrerit egestas lacus, vulputate pulvinar arcu volutpat vel. Donec tincidunt orci vel velit aliquam in dignissim nulla tempus. Donec cursus ipsum vel dolor porta adipiscing. Nulla nec auctor lectus. Curabitur vestibulum neque sed erat auctor ultricies.</p>
+
+<p>In nec imperdiet lectus. Duis eget orci eu diam tincidunt porttitor. In quam lectus, semper vitae euismod ac, adipiscing vitae turpis. Nam a euismod leo. Pellentesque eget nibh ut turpis porttitor pretium ut at augue. Vivamus at justo nisl, a laoreet sem. Suspendisse nibh sapien, accumsan in dictum vel, interdum a mi. Aenean et egestas magna. Donec aliquam, sapien quis gravida volutpat, dolor elit consequat lacus, ut cursus orci nibh vitae tortor. Curabitur rutrum vehicula placerat. Vivamus porta dignissim urna, sed vestibulum turpis euismod non. In non sapien eu eros faucibus interdum quis nec diam.</p>
+
+<p>Pellentesque mi ligula, tincidunt eget elementum vitae, sollicitudin a purus. Sed eget quam lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ut lacus at nisi sollicitudin mollis. Pellentesque eleifend metus vel nibh rhoncus adipiscing. Aliquam a erat laoreet quam luctus placerat eu ac magna. Praesent fermentum est in sapien pharetra vestibulum. Aenean enim velit, facilisis sed varius nec, tristique varius tellus. Quisque tortor dui, ultricies non suscipit aliquam, lobortis quis erat. Donec a arcu nisi, eleifend dictum tortor. Cras egestas lectus quis sapien pulvinar a adipiscing leo interdum. Integer imperdiet, metus in placerat scelerisque, turpis sem convallis justo, vitae gravida erat metus quis dolor. Quisque sed augue eget ligula viverra placerat. Aliquam ut arcu eget libero venenatis iaculis eu et purus.</p>
+
+<p>Nunc eget accumsan ligula. Praesent lorem dolor, pretium sit amet pellentesque sit amet, placerat vel est. Nullam tempor ornare aliquet. Maecenas scelerisque euismod arcu, vel ultrices diam porta vitae. Praesent elementum, ligula eu scelerisque eleifend, leo lectus luctus mauris, in luctus metus neque et massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas tincidunt consequat quam et molestie. Donec at nisl ipsum, ut fermentum orci. Nam a congue justo. Quisque magna nisl, vestibulum non venenatis sed, gravida ac lectus. Sed non felis non leo semper pellentesque.</p>
+
+<p>Mauris tellus ipsum, interdum vitae faucibus ut, iaculis nec risus. Vestibulum imperdiet auctor mollis. Nullam sit amet risus et nunc imperdiet euismod vitae eget felis. Nulla et eros dui, eget aliquet metus. Donec nec tincidunt lorem. Sed vitae arcu ante. Maecenas magna leo, feugiat ut convallis quis, tincidunt quis tortor. Etiam sed felis ac ligula eleifend porttitor porttitor in lectus. Pellentesque vulputate sem quis ligula placerat semper. Suspendisse vulputate, ante sed vulputate facilisis, massa arcu aliquet nisl, scelerisque posuere arcu tortor a tellus. Nunc egestas tempor euismod. Morbi scelerisque est ultrices neque congue tristique.</p>
+
+<p>In ut leo lacus. Nulla euismod elementum aliquam. Etiam vel nunc sit amet odio placerat rhoncus sed non augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce id felis augue, vitae iaculis leo. Fusce non dui ac sem rutrum imperdiet quis eu eros. Aenean libero dolor, bibendum iaculis dictum id, pharetra eu metus. Praesent pulvinar, ante in molestie ornare, odio lacus ultricies lorem, non condimentum metus nisi eu augue. Sed felis metus, tristique vel feugiat quis, pulvinar sit amet nulla. Praesent dignissim lacinia purus sed ultricies. Maecenas rhoncus risus id arcu dignissim bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed dictum urna id quam luctus at porttitor neque porta. Integer nec felis eu enim posuere euismod at at lectus. In hac habitasse platea dictumst.</p>
+
+<p>Donec porta dignissim est. Praesent vitae justo magna, nec porta dui. Sed euismod ipsum sit amet quam luctus a sollicitudin ipsum lacinia. Ut fermentum cursus ipsum, ultrices tristique turpis feugiat nec. Aenean ornare tristique sapien non consequat. Ut justo libero, consectetur ac eleifend eget, commodo in lorem. Phasellus quis nulla a quam malesuada sollicitudin nec ac ante. Phasellus vehicula scelerisque est id vehicula.</p>
+
+<p>Duis mollis, nulla a consectetur porttitor, lectus lacus tincidunt nulla, sed sagittis quam diam in lacus. Praesent aliquet dictum est, sit amet hendrerit lectus mattis semper. Nunc eu erat lectus. Nunc nec diam a libero hendrerit volutpat. In nec ipsum lorem, a feugiat tortor. Quisque pharetra convallis turpis, quis vehicula est posuere ut. Morbi vulputate pretium libero. Nam nec justo nec turpis rhoncus sodales. Proin cursus blandit metus eget dictum. Proin a sapien eros. Integer non purus non ipsum rhoncus pretium.</p>
+
+<p>Sed volutpat rhoncus orci a imperdiet. Etiam vulputate, erat ut viverra iaculis, odio erat commodo enim, sit amet euismod justo neque at est. Sed vitae eros id massa bibendum scelerisque facilisis ut dui. Donec sagittis tincidunt pharetra. Donec blandit consectetur justo, eu scelerisque justo ornare quis. Fusce a molestie sem. Mauris rhoncus posuere nunc cursus tristique. Suspendisse congue erat vel tellus laoreet pharetra. Donec dictum, nulla sed sodales venenatis, sem mauris venenatis justo, eget mollis nisl elit fringilla nulla. Donec sollicitudin, urna eu auctor luctus, sapien nunc tincidunt lacus, non tempus nulla purus sed erat.</p>
+
+<p>Morbi sed ipsum a nibh suscipit convallis at ut libero. Fusce sodales sollicitudin tortor sed aliquam. Nunc non lacus ac augue porta scelerisque. Ut placerat, dolor eget ullamcorper luctus, nisi purus aliquet arcu, id varius dui mi ac sem. In vitae tincidunt orci. Donec sed convallis dolor. Donec ac imperdiet mauris. Duis mollis tempus accumsan. Nam diam tortor, scelerisque id convallis sed, consectetur non velit. Nulla facilisi. Morbi commodo mattis ante sit amet tincidunt. Etiam mollis turpis vitae magna elementum cursus. Donec hendrerit faucibus ullamcorper. Nam porttitor hendrerit imperdiet. Quisque nec dapibus leo. Mauris quis velit mauris, et ornare quam.</p>
+</div>
+
+<div style="float:right;width:50%;margin:0;padding:0;">
+Here's an image:
+<img id="guid-of-image" src="http://d2gk67vhg5dnwa.cloudfront.net/image/jellyfish.jpg" class="imageasset" usemap= "#image_map" border= "0"/>
+<video Poster=" http://d2gk67vhg5dnwa.cloudfront.net/image/jellyfish.jpg"  loop controls tabindex="0">
+<source src="http://mmsdemo2.llnw.net/o1/reach/source-videos/lldemo2/atlantis.mov">
+<mapname="image_map">
+  <area rel="prettyPhoto[image_map] shape="rect" coords 0>
+  </map>
+</div>
+<div style="clear:both"></div>
+
+<div id="guid-of-video" style="border: 2px inset; margin:30" class="videoasset" url="http://mmsdemo2.llnw.net/o1/reach/source-videos/lldemo2/atlantis.mov">
+Replace this with video player to url
+</div>
+
+
+/*<div id="guid-of-audio" style="border: 2px inset; margin:30" class="audioasset" url="http://d2gk67vhg5dnwa.cloudfront.net/audio/test.mp3">
+Replace this with audio player to url
+</div>
+
+<div id="guid-of-flash" style="border: 2px inset; margin:30" class="flashasset" url="http://d2gk67vhg5dnwa.cloudfront.net/interactive/CourseOrganizer_Final.swf">
+Replace this with flash player to url
+</div>
+
+</body>
+</html>
